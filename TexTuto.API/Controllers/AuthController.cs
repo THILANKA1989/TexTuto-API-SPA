86bdd48517f2af56aed47b0e9845ca461e726d52
Thilanka Ranasinghe <THILANKA1989@users.noreply.github.com>
2019-07-10 15:02:09 +0530

    Added auth controller

---
+++ TexTuto.API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+namespace TexTuto.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo){
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password){
+            username = username.ToLower();
+
+            if (await _repo.UserExists(username)){
+                return BadRequest("Username already exist");
+            }
+
+            var userToCreate = new User{
+                username = username
+            };
+
+            var createdUser = await _repo.Register(userToCreate,password);
+
+            return StatusCode(201);
+        }
+    }
+}
