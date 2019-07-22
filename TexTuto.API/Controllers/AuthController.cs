19ed01eefc7aabc3a9875562b878bc1fc234b927
Thilanka Ranasinghe <THILANKA1989@users.noreply.github.com>
2019-07-22 13:52:36 +0530

    cimmit

--- TexTuto.API/Controllers/AuthController.cs
+++ TexTuto.API/Controllers/AuthController.cs
@@ -1,4 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using TexTuto.API.Data;
 using TexTuto.API.DTO;
@@ -12,13 +18,20 @@ namespace TexTuto.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(IAuthRepository repo){
+        public AuthController(IAuthRepository repo, IConfiguration configuration){
             _repo = repo;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto){
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDto.Username)){
@@ -32,6 +45,44 @@ namespace TexTuto.API.Controllers
             var createdUser = await _repo.Register(userToCreate,userForRegisterDto.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            //fetch user form credintials
+            User userFromRepo = await _repo.Login(userForLoginDto.Username , userForLoginDto.Password );
+            if(userFromRepo == null)
+                return Unauthorized();
+
+            //create claims with fetched user informations
+            var claims = new[]
+            {
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.username)
+            };
+
+            //create security key by using Secret token in appsettings
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            //assign the security algorithm generate credentials
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
